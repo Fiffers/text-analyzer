fcfe606bc0650c912200aa9a759bc42b68492b0a
sjpfeiffer <stevenjamespfeiffer@gmail.com>
2016-10-16 11:14:34 +0430

    v2
    Fixed average length of sentences to show length in words instead of characters.

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 $(document).ready(function(event) {
-	var wordLength, totalWordLength = 0;
-	var sentenceLength, totalSentenceLength = 0;
-	var uniqueWords = 0
 	$('form').submit(function(event) {
+
+		//When form is submitted, reset all values to 0.
+		var wordLength, totalWordLength = 0;
+		var sentenceLength, totalSentenceLength = 0;
+		var uniqueWords, wordCount = 0
+		var sentenceCount = 0
+			
+		//Prevents the page from reloading upon form submission.
 		event.preventDefault();
-		var wordCount = ($('textarea#user-text').val().split(' '));
-		var uniqueWords = ($('textarea#user-text').val().split(' '));
-		var sentenceCount = ($('textarea#user-text').val().split('.'));
+
+		//Store all the major values I need for
+		//the function into an array with the array
+		//split by periods and spaces
+		wordCount = ($('textarea#user-text').val().split(' '));
+		uniqueWords = ($('textarea#user-text').val().split(' '));
+		sentenceCount = ($('textarea#user-text').val().split('. '));
+		//console.log (sentenceCount)
+
+		//Loop through wordCount[i].length
+		//to get total # of words.
 		for (var i = 0; i < wordCount.length; i++) {
 			totalWordLength += wordCount[i].length;
 		}
+
+		//Loop through sentenceCount[i].length
+		//to get total # of sentences.
 		for (var i = 0; i < sentenceCount.length; i++) {
-			totalSentenceLength += sentenceCount[i].length;
+			totalSentenceLength += sentenceCount[i].split(' ').length;
+			console.log (totalSentenceLength);
 		}
-		uniqueWords = $.unique(uniqueWords);
+
+		//searches through the array, sorts the array, and 
+		//removes duplicates. My Google-Fu is strong.
+		uniqueWords = jQuery.uniqueSort(uniqueWords);
+
+		//Gets the average I need to get the average 
+		//length of all the words submitted.
 		wordLength = (totalWordLength / wordCount.length);
+
+		//Gets the average I need to get the average 
+		//length of all the sentences submitted.
 		sentenceLength = (totalSentenceLength / sentenceCount.length);
+
+		//Removes hidden class from the parent <dl>
 		$('.js-text-report').removeClass('hidden');
+
+		//Adds the stored variables 
+		//to the <dd> elements as text.
 		$('.js-total-count').text(wordCount.length);
 		$('.js-unique-count').text(uniqueWords.length);
 		$('.js-average-word-length').text(wordLength);
